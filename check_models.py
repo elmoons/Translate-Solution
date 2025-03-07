3f6f64bc79c1ef9e430833f123c5a9bd467435db
eLmoons <elmoons@yandex.ru>
2025-03-07 12:10:13 +0300

    testing new languages

--- check_models.py
+++ check_models.py
@@ -2,13 +2,41 @@ from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 MODEL_DIR_EN_RU = "models/opus-mt-en-ru"
 MODEL_DIR_RU_EN = "models/opus-mt-ru-en"
+MODEL_DIR_LV_RU = "models/opus-mt-lv-ru"
+MODEL_DIR_RU_LV = "models/opus-mt-ru-lv"
+MODEL_DIR_ET_RU = "models/opus-mt-et-ru"
+MODEL_DIR_RU_ET = "models/opus-mt-ru-et"
+MODEL_DIR_NO_RU = "models/opus-mt-no-ru"
+MODEL_DIR_RU_NO = "models/opus-mt-ru-no"
 
 try:
+    # Английский
     tokenizer_en_ru = AutoTokenizer.from_pretrained(MODEL_DIR_EN_RU, local_files_only=True)
     model_en_ru = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_EN_RU, local_files_only=True)
 
     tokenizer_ru_en = AutoTokenizer.from_pretrained(MODEL_DIR_RU_EN, local_files_only=True)
     model_ru_en = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_RU_EN, local_files_only=True)
+
+    # Латышский
+    tokenizer_lv_ru = AutoTokenizer.from_pretrained(MODEL_DIR_LV_RU, local_files_only=True)
+    model_lv_ru = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_LV_RU, local_files_only=True)
+
+    tokenizer_ru_lv = AutoTokenizer.from_pretrained(MODEL_DIR_RU_LV, local_files_only=True)
+    model_ru_lv = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_RU_LV, local_files_only=True)
+
+    # Эстонский
+    tokenizer_et_ru = AutoTokenizer.from_pretrained(MODEL_DIR_ET_RU, local_files_only=True)
+    model_et_ru = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_ET_RU, local_files_only=True)
+
+    tokenizer_ru_et = AutoTokenizer.from_pretrained(MODEL_DIR_RU_ET, local_files_only=True)
+    model_ru_et = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_RU_ET, local_files_only=True)
+
+    # Норвежский
+    tokenizer_no_ru = AutoTokenizer.from_pretrained(MODEL_DIR_NO_RU, local_files_only=True)
+    model_no_ru = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_NO_RU, local_files_only=True)
+
+    tokenizer_ru_no = AutoTokenizer.from_pretrained(MODEL_DIR_RU_NO, local_files_only=True)
+    model_ru_no = AutoModelForSeq2SeqLM.from_pretrained(MODEL_DIR_RU_NO, local_files_only=True)
 
     print("✅ Модели загружены успешно!")
 except Exception as e:
--- process.py
+++ process.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from models import translate_from_en_to_ru, translate_from_ru_to_en
+from models import translate_from_en_to_ru, translate_from_ru_to_en, translate_from_lv_to_ru, translate_from_ru_to_lv, \
+    translate_from_et_to_ru, translate_from_ru_to_et, translate_from_no_to_ru, translate_from_ru_to_no
 
 
 def process_excel(input_path: str, output_path: str, language: str, direction: str):
@@ -11,14 +12,26 @@ def process_excel(input_path: str, output_path: str, language: str, direction: s
     indexes = words.index  # Индексы непустых строк
 
     # Переводим слова
+    # Английский
     if direction == "на Русский" and language == "Английский":
         translated_words = translate_from_en_to_ru(words.tolist())
     elif direction == "на выбранный" and language == "Английский":
         translated_words = translate_from_ru_to_en(words.tolist())
+    # Латышский
     elif direction == "на Русский" and language == "Латышский":
         translated_words = translate_from_lv_to_ru(words.tolist())
     elif direction == "на выбранный" and language == "Латышский":
         translated_words = translate_from_ru_to_lv(words.tolist())
+    # Эстонский
+    elif direction == "на Русский" and language == "Эстонский":
+        translated_words = translate_from_et_to_ru(words.tolist())
+    elif direction == "на выбранный" and language == "Эстонский":
+        translated_words = translate_from_ru_to_et(words.tolist())
+    # Норвежский
+    elif direction == "на Русский" and language == "Норвежский":
+        translated_words = translate_from_no_to_ru(words.tolist())
+    elif direction == "на выбранный" and language == "Норвежский":
+        translated_words = translate_from_ru_to_no(words.tolist())
 
     # Вставляем перевод обратно с сохранением индексов
     df_input.loc[indexes, 1] = translated_words
